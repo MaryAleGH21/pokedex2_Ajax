8641b61d7c464d713efd10fc0f01cbda1c9a64b6
Alejandr Serrano <marialesr.17@gmail.com>
2021-04-13 12:52:49 -0600

    More pokemon added

--- assets/js/pokemones.js
+++ assets/js/pokemones.js
@@ -1,20 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
   const state = {
-    pokemons: []
+    pokemons: [],
+    offset: 0
   }
   const pokemons = document.querySelector('ul.pokemons')
 
+  document.addEventListener('click', function(e){
+    if (e.target.matches('[href="#"]')){
+      e.preventDefault()
+    }
+
+    if (e.target.matches('.more-pokemons')){
+      fetchPokemons()
+    }
+  })
+
   function fetchPokemons(){
-    fetch('https://pokeapi.co/api/v2/pokemon/', {
+    fetch('https://pokeapi.co/api/v2/pokemon/' + `?offset=${state.offset}`, {
       method: 'GET', 
       headers: {
         'Content-Type': 'application/json'
       }
     }).then(function(data){ return data.json()})
-      .then(function(pokemons){
-      state.pokemons = pokemons.results
-      renderPokemons()
-    })
+      .then(function(pokemons){addPokemons(pokemons.results)})
+      .then(function(){ state.offset += 20})
+    }
+
+  function addPokemons(pokemons){
+    state.pokemons = [...state.pokemons,...pokemons] //Los tres puntos rompen el array existente y los ponen en uno nuevo.
+    renderPokemons()
   }
 
   function renderPokemons(){
