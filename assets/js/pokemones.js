6dac1fa5f98495e393e862ec5e7e961113e8fc3b
Alejandr Serrano <marialesr.17@gmail.com>
2021-04-13 16:12:52 -0600

    adding new functions

--- assets/js/pokemones.js
+++ assets/js/pokemones.js
@@ -13,6 +13,16 @@ document.addEventListener('DOMContentLoaded', function(){
     if (e.target.matches('.more-pokemons')){
       fetchPokemons()
     }
+
+    if (e.target.matches('.pokemon-detail')){
+      e.preventDefault()
+     return fectchPokemon(e.target.href)
+    }
+
+    if (e.target.closest('.pokemon-detail')){
+      e.preventDefault()
+      fectchPokemon(e.target.parentNode.href)
+    }
   })
 
   function fetchPokemons(){
@@ -26,13 +36,70 @@ document.addEventListener('DOMContentLoaded', function(){
       .then(function(){ state.offset += 20})
     }
 
+    function fectchPokemon(url){
+      fetch(url)
+        .then(function(data){ return data.json()})
+        .then(function(pokemon){renderModal(pokemon)})
+    }
+
   function addPokemons(pokemons){
     state.pokemons = [...state.pokemons,...pokemons] //Los tres puntos rompen el array existente y los ponen en uno nuevo.
     renderPokemons()
   }
 
+  function renderModal(poke){
+     console.log(poke)
+    const poke_modal = document.createElement('div')
+    poke_modal.className = 'modal fade'
+
+    const poke_types = poke.types.map(function(p){ return `<li>${p.type.name}</li>`})
+    const poke_abilities = poke.abilities.map(function(a){ return `<li>${a.ability.name}</li>`})
+    const poke_movements =  poke.moves.slice(0,5).map(function(poke){return `<li>${poke.move.name}</li>`}) 
+    
+    const html = `<div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header border-0">
+                <h5 class="modal-title" id="pokemon-data-name">${poke.name}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body px-0">
+                <table class="table">
+                    <tr>
+                        <th>Tipos</th>
+                        <td>
+                            <ol id="pokemon-types">${poke_types.join('')}</ol>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Habilidades</th>
+                        <td>
+                            <ol id="pokemon-abilities">${poke_abilities.join('')}</ol>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>5 primeros movimientos</th>
+                        <td>
+                            <ol id="pokemon-moves">${poke_movements.join('')}</ol>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>`
+
+      poke_modal.innerHTML = html
+      document.body.appendChild(poke_modal)
+
+      $('.modal').modal('show')
+      $('.modal').on('hidden.bs.modal', function(){ //'bs' Boostrap
+        $(this).remove()
+      })
+  }
+
   function renderPokemons(){
-    state.pokemons.map(function(poke){
+    state.pokemons.slice(state.offset).map(function(poke){
       const li = document.createElement('li')
       li.className = 'list-group-item'
 
